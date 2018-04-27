4c698d17d90ceb6cdb2607a118482d86cbe1c5eb
Ryan Uber <ru@ryanuber.com>
2018-04-27 15:49:08 -0700

    Testing configuration version creation

--- configuration_version_test.go
+++ configuration_version_test.go
@@ -31,3 +31,46 @@ func TestListConfigurationVersions(t *testing.T) {
 	assert.Contains(t, found, *cv1.ID)
 	assert.Contains(t, found, *cv2.ID)
 }
+
+func TestCreateConfigurationVersion(t *testing.T) {
+	client := testClient(t)
+
+	ws, wsCleanup := createWorkspace(t, client, nil)
+	defer wsCleanup()
+
+	t.Run("with valid input", func(t *testing.T) {
+		input := &CreateConfigurationVersionInput{
+			WorkspaceID: ws.ID,
+		}
+		resp, err := client.CreateConfigurationVersion(input)
+		require.Nil(t, err)
+
+		// Get a refreshed view of the configuration version.
+		refreshed, err := client.ConfigurationVersion(*resp.ConfigurationVersion.ID)
+		require.Nil(t, err)
+
+		for _, cv := range []*ConfigurationVersion{
+			resp.ConfigurationVersion,
+			refreshed,
+		} {
+			assert.NotNil(t, cv.ID)
+			// TODO: Fix this. API does not return workspace associations.
+			//assert.Equal(t, input.WorkspaceID, cv.WorkspaceID)
+			assert.NotNil(t, cv.UploadURL)
+			assert.NotEqual(t, 0, len(*cv.UploadURL))
+			assert.Equal(t, *cv.Status, "pending")
+			assert.Equal(t, *cv.Source, "tfe-api")
+			assert.Nil(t, cv.Error)
+		}
+	})
+
+	t.Run("with invalid workspace id", func(t *testing.T) {
+		result, err := client.CreateConfigurationVersion(
+			&CreateConfigurationVersionInput{
+				WorkspaceID: String("! / nope"),
+			},
+		)
+		assert.Nil(t, result)
+		assert.EqualError(t, err, "Invalid value for WorkspaceID")
+	})
+}
