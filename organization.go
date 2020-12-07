b0e468023709a9d44ff835c7b22200f4bc20c8cc
Chris Arcand <chris@chrisarcand.com>
2020-12-06 21:26:26 -0600

    Add additional organization entitlements
    These entitlements were added to the API since the original struct was
    written.
    
    Note that not all attributes that exist today were added here; some
    attributes are all but deprecated as they are always true now, for
    legacy reasons (the configuration designer is an example).

--- organization.go
+++ organization.go
@@ -101,8 +101,12 @@ type Capacity struct {
 // Entitlements represents the entitlements of an organization.
 type Entitlements struct {
 	ID                    string `jsonapi:"primary,entitlement-sets"`
+	Agents                bool   `jsonapi:"attr,agents"`
+	AuditLogging          bool   `jsonapi:"attr,audit-logging"`
+	CostEstimation        bool   `jsonapi:"attr,cost-estimation"`
 	Operations            bool   `jsonapi:"attr,operations"`
 	PrivateModuleRegistry bool   `jsonapi:"attr,private-module-registry"`
+	SSO                   bool   `jsonapi:"attr,sso"`
 	Sentinel              bool   `jsonapi:"attr,sentinel"`
 	StateStorage          bool   `jsonapi:"attr,state-storage"`
 	Teams                 bool   `jsonapi:"attr,teams"`
