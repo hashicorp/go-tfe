49abf18b9338393c1e7977382caf523b5dc6dfa0
Alex Hess <alex.hess@snowflake.com>
2021-01-14 12:30:06 -0500

    change to simpler unmarshaler interface

--- state_version_output.go
+++ state_version_output.go
@@ -1,7 +1,6 @@
 package tfe
 
 import (
-	"bytes"
 	"context"
 	"encoding/json"
 	"errors"
@@ -19,8 +18,6 @@ var _ StateVersionOutputs = (*stateVersionOutputs)(nil)
 // TFE API docs: https://www.terraform.io/docs/cloud/api/state-version-outputs.html
 type StateVersionOutputs interface {
 	Read(ctx context.Context, outputID string) (*StateVersionOutput, error)
-
-	ReadSimpler(ctx context.Context, outputID string) (*StateVersionOutputAllKinds, error)
 }
 
 type stateVersionOutputs struct {
@@ -32,17 +29,18 @@ type StateVersionOutput struct {
 	Name      string `jsonapi:"attr,name"`
 	Sensitive bool   `jsonapi:"attr,sensitive"`
 	Type      string `jsonapi:"attr,type"`
-	Value     string `jsonapi:"attr,value"`
+	Value     OutputValue `jsonapi:"attr,value"`
 }
 
-type StateVersionOutputAllKinds struct {
-	ID        string `jsonapi:"primary,state-version-outputs"`
-	Name      string `jsonapi:"attr,name"`
-	Sensitive bool   `jsonapi:"attr,sensitive"`
-	Type      string `jsonapi:"attr,type"`
-	Value     interface{} `jsonapi:"attr,value"`
+// Since the Output can be one of many types, and we don't want to use interface{} here, this type
+// can store all types of output.   There may be more types that are not yet implemented here.
+type OutputValue struct {
+	ValueBool 	bool
+	ValueInt	int
+	ValueString	string
+	ValueArray	[]interface{}
+	ValueMap	map[string]interface{}
 }
-
 
 func (s *stateVersionOutputs) Read(ctx context.Context, outputID string) (*StateVersionOutput, error) {
 	if !validStringID(&outputID) {
@@ -64,55 +62,30 @@ func (s *stateVersionOutputs) Read(ctx context.Context, outputID string) (*State
 	return so, nil
 }
 
-// This is here because the original implementation, above, does not support State Outputs that are not strings, and errors when you try to read one.
-// State outputs can also be arrays, as in our use case.  So we support the interface{} type for any data type, and the 'type' field can be used to determine what kind of data you have.
-// The underlying JSON API parser does not support this type of changeable output, or so it would seem, so we shall use a more raw approach.
-func (s *stateVersionOutputs) ReadSimpler(ctx context.Context, outputID string) (*StateVersionOutputAllKinds, error) {
-	if !validStringID(&outputID) {
-		return nil, errors.New("invalid value for run ID")
-	}
-
-	u := fmt.Sprintf("state-version-outputs/%s", url.QueryEscape(outputID))
-	req, err := s.client.newRequest("GET", u, nil)
+// Allow the 'value' component of this state output to be unmarshaled to the right kind of data.
+func (ov *OutputValue) UnmarshalJSON(b []byte) error {
+	// unmarshal the data and then store it.
+	var result interface{}
+	err := json.Unmarshal(b, &result)
 	if err != nil {
-		return nil, err
+		return err
 	}
 
-	var buf bytes.Buffer
-	err = s.client.do(ctx, req, &buf)
-	if err != nil {
-		return nil, err
+	// Test what kind of data we have here and store it in the right place.
+	switch v := result.(type) {
+	case int:
+		ov.ValueInt = v
+	case string:
+		ov.ValueString = v
+	case bool:
+		ov.ValueBool = v
+	case []interface{}:
+		ov.ValueArray = v
+	case map[string]interface{}:
+		ov.ValueMap = v
+	default:
+		return fmt.Errorf("unknown output type: %v", v)
 	}
-
-	// Convert to a json map
-	var result map[string]interface{}
-	err = json.Unmarshal(buf.Bytes(), &result)
-	if err != nil {
-		return nil, err
-	}
-
-	// Create a new value to hold our output
-	so := &StateVersionOutputAllKinds{}
-	so.ID = outputID
-
-	// Read in the data from the json.
-	// Look for an object called Data, and below that, attributes..
-	if data, ok := result["data"].(map[string]interface{}); ok {
-		if attr, ok := data["attributes"].(map[string]interface{}); ok {
-			if name, ok := attr["name"].(string); ok {
-				so.Name = name
-			}
-			if sensitive, ok := attr["name"].(bool); ok {
-				so.Sensitive = sensitive
-			}
-			if tp, ok := attr["type"].(string); ok {
-				so.Type = tp
-			}
-			if value, ok := attr["value"]; ok {
-				so.Value = value
-			}
-		}
-	}
-
-	return so, nil
+	return nil
 }
+
