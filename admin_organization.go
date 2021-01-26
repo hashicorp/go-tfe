d4396828f277271a158939188ecfc7dee98a71fe
Karl Kirch <karl@hashicorp.com>
2021-01-26 16:28:39 -0600

    Add admin organization patch/get endpoints
    Add admin organization tests

--- admin_organization.go
+++ admin_organization.go
@@ -2,6 +2,7 @@ package tfe
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"net/url"
 )
@@ -21,6 +22,12 @@ type AdminOrganizations interface {
 
 	// Update the module sharing partnerships that an organization has
 	UpdateModuleConsumers(ctx context.Context, organization string, options ModulePartnershipUpdateOptions) (*ModulePartnershipList, error)
+
+	// Read attributes of an existing organization via admin API.
+	Read(ctx context.Context, organization string) (*AdminOrganization, error)
+
+	// Update attributes of an existing organization via admin API.
+	Update(ctx context.Context, organization string, options AdminOrganizationUpdateOptions) (*AdminOrganization, error)
 }
 
 // adminOrganizations implements AdminOrganizations.
@@ -47,7 +54,35 @@ type ModulePartnershipUpdateOptions struct {
 	ModuleConsumingOrganizationIDs []*string `jsonapi:"attr,module-consuming-organization-ids"`
 }
 
+// AdminOrganization represents a Terraform Enterprise organization returned from the Admin API.
+type AdminOrganization struct {
+	Name string `jsonapi:"primary,organizations"`
+
+	AccessBetaTools                  bool   `jsonapi:"attr,access-beta-tools"`
+	ExternalID                       string `jsonapi:"attr,external-id"`
+	GlobalModuleSharing              bool   `jsonapi:"attr,global-module-sharing"`
+	IsDisabled                       bool   `jsonapi:"attr,is-disabled"`
+	NotificationEmail                string `jsonapi:"attr,notification-email"`
+	SsoEnabled                       bool   `jsonapi:"attr,sso-enabled"`
+	TerraformBuildWorkerApplyTimeout string `jsonapi:"attr,terraform-build-worker-apply-timeout"`
+	TerraformBuildWorkerPlanTimeout  string `jsonapi:"attr,terraform-build-worker-plan-timeout"`
+}
+
+// AdminOrganizationUpdateOptions represents the admin options for updating an organization.
+// https://www.terraform.io/docs/cloud/api/admin/organizations.html#request-body
+type AdminOrganizationUpdateOptions struct {
+	AccessBetaTools                  *bool   `jsonapi:"attr,access-beta-tools,omitempty"`
+	GlobalModuleSharing              *bool   `jsonapi:"attr,global-module-sharing,omitempty"`
+	IsDisabled                       *bool   `jsonapi:"attr,is-disabled,omitempty"`
+	TerraformBuildWorkerApplyTimeout *string `jsonapi:"attr,terraform-build-worker-apply-timeout,omitempty"`
+	TerraformBuildWorkerPlanTimeout  *string `jsonapi:"attr,terraform-build-worker-plan-timeout,omitempty"`
+}
+
 func (s *adminOrganizations) ListModuleConsumers(ctx context.Context, organization string) (*ModulePartnershipList, error) {
+	if !validStringID(&organization) {
+		return nil, errors.New("invalid value for organization")
+	}
+
 	u := fmt.Sprintf("admin/organizations/%s/module-consumers", url.QueryEscape(organization))
 	req, err := s.client.newRequest("GET", u, nil)
 	if err != nil {
@@ -64,6 +99,10 @@ func (s *adminOrganizations) ListModuleConsumers(ctx context.Context, organizati
 }
 
 func (s *adminOrganizations) UpdateModuleConsumers(ctx context.Context, organization string, options ModulePartnershipUpdateOptions) (*ModulePartnershipList, error) {
+	if !validStringID(&organization) {
+		return nil, errors.New("invalid value for organization")
+	}
+
 	u := fmt.Sprintf("admin/organizations/%s/module-consumers", url.QueryEscape(organization))
 	req, err := s.client.newRequest("PATCH", u, &options)
 	if err != nil {
@@ -77,4 +116,44 @@ func (s *adminOrganizations) UpdateModuleConsumers(ctx context.Context, organiza
 	}
 
 	return partnerships, nil
+}
+
+func (s *adminOrganizations) Read(ctx context.Context, organization string) (*AdminOrganization, error) {
+	if !validStringID(&organization) {
+		return nil, errors.New("invalid value for organization")
+	}
+
+	u := fmt.Sprintf("admin/organizations/%s", url.QueryEscape(organization))
+	req, err := s.client.newRequest("GET", u, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	org := &AdminOrganization{}
+	err = s.client.do(ctx, req, org)
+	if err != nil {
+		return nil, err
+	}
+
+	return org, nil
+}
+
+func (s *adminOrganizations) Update(ctx context.Context, organization string, options AdminOrganizationUpdateOptions) (*AdminOrganization, error) {
+	if !validStringID(&organization) {
+		return nil, errors.New("invalid value for organization")
+	}
+
+	u := fmt.Sprintf("admin/organizations/%s", url.QueryEscape(organization))
+	req, err := s.client.newRequest("PATCH", u, &options)
+	if err != nil {
+		return nil, err
+	}
+
+	org := &AdminOrganization{}
+	err = s.client.do(ctx, req, org)
+	if err != nil {
+		return nil, err
+	}
+
+	return org, nil
 }
--- admin_organization_test.go
+++ admin_organization_test.go
@@ -38,3 +38,67 @@ func TestModulePartnershipsList(t *testing.T) {
 		assert.Empty(t, consumerList.Items)
 	})
 }
+
+func TestAdminOrganizations(t *testing.T) {
+
+	skipIfNotEnterprise(t)
+
+	client := testClient(t)
+	ctx := context.Background()
+
+	org, orgTestCleanup := createOrganization(t, client)
+	defer orgTestCleanup()
+
+	t.Run("fetches and updates organization", func(t *testing.T) {
+		adminOrg, err := client.Admin.Organizations.Read(ctx, org.Name)
+		assert.NotNilf(t, adminOrg, "Org returned as nil")
+		assert.Nilf(t, err, "Failed to update org %v", err)
+
+		accessBetaTools := true
+		globalModuleSharing := true
+		isDisabled := false
+		terraformBuildWorkerApplyTimeout := "24h"
+		terraformBuildWorkerPlanTimeout := "24h"
+
+		opts := AdminOrganizationUpdateOptions{
+			AccessBetaTools:                  &accessBetaTools,
+			GlobalModuleSharing:              &globalModuleSharing,
+			IsDisabled:                       &isDisabled,
+			TerraformBuildWorkerApplyTimeout: &terraformBuildWorkerApplyTimeout,
+			TerraformBuildWorkerPlanTimeout:  &terraformBuildWorkerPlanTimeout,
+		}
+
+		adminOrg, err = client.Admin.Organizations.Update(ctx, org.Name, opts)
+
+		assert.NotNilf(t, adminOrg, "Org returned as nil")
+		assert.Nilf(t, err, "Failed to update org %v", err)
+
+		assert.Equal(t, adminOrg.AccessBetaTools, accessBetaTools)
+		assert.Equal(t, adminOrg.GlobalModuleSharing, globalModuleSharing)
+		assert.Equal(t, adminOrg.IsDisabled, isDisabled)
+		assert.Equal(t, adminOrg.TerraformBuildWorkerApplyTimeout, terraformBuildWorkerApplyTimeout)
+		assert.Equal(t, adminOrg.TerraformBuildWorkerPlanTimeout, terraformBuildWorkerPlanTimeout)
+
+		isDisabled = true
+		opts = AdminOrganizationUpdateOptions{
+			IsDisabled: &isDisabled,
+		}
+
+		adminOrg, err = client.Admin.Organizations.Update(ctx, org.Name, opts)
+		assert.NotNilf(t, adminOrg, "Org returned as nil")
+		assert.Nilf(t, err, "Failed to update org %v", err)
+
+		assert.Equal(t, adminOrg.IsDisabled, isDisabled)
+
+		isDisabled = false
+		opts = AdminOrganizationUpdateOptions{
+			IsDisabled: &isDisabled,
+		}
+
+		adminOrg, err = client.Admin.Organizations.Update(ctx, org.Name, opts)
+		assert.NotNilf(t, adminOrg, "Org returned as nil")
+		assert.Nilf(t, err, "Failed to update org %v", err)
+
+		assert.Equal(t, adminOrg.IsDisabled, isDisabled)
+	})
+}
