ad197f504c350156b106b2a3d9b8d8f9bb5017a2
Jonas-Taha El Sesiy <github@elsesiy.com>
2021-08-17 23:26:09 -0700

    Update state-version outputs for complex types
    Replace string with json.RawMessaget to account for non-string state outputs

--- state_version_output.go
+++ state_version_output.go
@@ -2,6 +2,7 @@ package tfe
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"net/url"
 )
@@ -23,11 +24,11 @@ type stateVersionOutputs struct {
 }
 
 type StateVersionOutput struct {
-	ID        string `jsonapi:"primary,state-version-outputs"`
-	Name      string `jsonapi:"attr,name"`
-	Sensitive bool   `jsonapi:"attr,sensitive"`
-	Type      string `jsonapi:"attr,type"`
-	Value     string `jsonapi:"attr,value"`
+	ID        string          `jsonapi:"primary,state-version-outputs"`
+	Name      string          `jsonapi:"attr,name"`
+	Sensitive bool            `jsonapi:"attr,sensitive"`
+	Type      string          `jsonapi:"attr,type"`
+	Value     json.RawMessage `jsonapi:"attr,value"`
 }
 
 func (s *stateVersionOutputs) Read(ctx context.Context, outputID string) (*StateVersionOutput, error) {
