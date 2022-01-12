14decd2f04da19ef1996d0c16d51a5e46e220a45
Sebastian Rivera <sebastian.rivera@hashicorp.com>
2022-01-12 13:18:18 -0500

    Removed hardcoded values, using test helpers to test e2e

--- task_stages_integration_test.go
+++ task_stages_integration_test.go
@@ -12,11 +12,28 @@ func TestTaskStagesRead(t *testing.T) {
 	client := testClient(t)
 	ctx := context.Background()
 
-	// hardcoded currently
-	taskStageID := "ts-xzskdpGj36B4ZJGn"
+	orgTest, orgTestCleanup := createOrganization(t, client)
+	defer orgTestCleanup()
+
+	runTaskTest, runTaskTestCleanup := createRunTask(t, client, orgTest)
+	defer runTaskTestCleanup()
+
+	wkspaceTest, wkspaceTestCleanup := createWorkspace(t, client, orgTest)
+	defer wkspaceTestCleanup()
+
+	wrTaskTest, wrTaskTestCleanup := createWorkspaceRunTask(t, client, wkspaceTest, runTaskTest)
+	defer wrTaskTestCleanup()
+
+	rTest, rTestCleanup := createRun(t, client, wkspaceTest)
+	defer rTestCleanup()
 
 	t.Run("without include param", func(t *testing.T) {
-		taskStage, err := client.TaskStages.Read(ctx, taskStageID, nil)
+		r, err := client.Runs.ReadWithOptions(ctx, rTest.ID, &RunReadOptions{
+			Include: "task_stages",
+		})
+		require.NoError(t, err)
+
+		taskStage, err := client.TaskStages.Read(ctx, r.TaskStages[0].ID, nil)
 		require.NoError(t, err)
 
 		assert.NotEmpty(t, taskStage.ID)
@@ -36,14 +53,22 @@ func TestTaskStagesRead(t *testing.T) {
 	})
 
 	t.Run("with include param task_results", func(t *testing.T) {
-		taskStage, err := client.TaskStages.Read(ctx, taskStageID, &TaskStageReadOptions{
+		r, err := client.Runs.ReadWithOptions(ctx, rTest.ID, &RunReadOptions{
+			Include: "task_stages",
+		})
+		require.NoError(t, err)
+
+		taskStage, err := client.TaskStages.Read(ctx, r.TaskStages[0].ID, &TaskStageReadOptions{
 			Include: "task_results",
 		})
 		require.NoError(t, err)
 
 		t.Run("task results are properly decoded", func(t *testing.T) {
+			assert.NotEmpty(t, taskStage.TaskResults[0].ID)
 			assert.NotEmpty(t, taskStage.TaskResults[0].Status)
-			assert.NotEmpty(t, taskStage.TaskResults[0].Message)
+			assert.NotEmpty(t, taskStage.TaskResults[0].CreatedAt)
+			assert.Equal(t, wrTaskTest.ID, taskStage.TaskResults[0].WorkspaceTaskID)
+			assert.Equal(t, runTaskTest.Name, taskStage.TaskResults[0].TaskName)
 		})
 	})
 }
@@ -52,13 +77,33 @@ func TestTaskStagesList(t *testing.T) {
 	client := testClient(t)
 	ctx := context.Background()
 
-	runID := "run-TRdorPvcxENJ5t52"
+	orgTest, orgTestCleanup := createOrganization(t, client)
+	defer orgTestCleanup()
+
+	runTaskTest, runTaskTestCleanup := createRunTask(t, client, orgTest)
+	defer runTaskTestCleanup()
+
+	runTaskTest2, runTaskTest2Cleanup := createRunTask(t, client, orgTest)
+	defer runTaskTest2Cleanup()
+
+	wkspaceTest, wkspaceTestCleanup := createWorkspace(t, client, orgTest)
+	defer wkspaceTestCleanup()
+
+	_, wrTaskTestCleanup := createWorkspaceRunTask(t, client, wkspaceTest, runTaskTest)
+	defer wrTaskTestCleanup()
+
+	_, wrTaskTest2Cleanup := createWorkspaceRunTask(t, client, wkspaceTest, runTaskTest2)
+	defer wrTaskTest2Cleanup()
+
+	rTest, rTestCleanup := createRun(t, client, wkspaceTest)
+	defer rTestCleanup()
 
 	t.Run("with no params", func(t *testing.T) {
-		taskStageList, err := client.TaskStages.List(ctx, runID, nil)
+		taskStageList, err := client.TaskStages.List(ctx, rTest.ID, nil)
 		require.NoError(t, err)
 
 		assert.NotNil(t, taskStageList.Items)
 		assert.NotEmpty(t, taskStageList.Items[0].ID)
+		assert.Equal(t, 2, len(taskStageList.Items[0].TaskResults))
 	})
 }
