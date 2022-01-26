349dd3c38b48f268df0dbb66c3e8c5b5bbdd5e90
Jonas-Taha El Sesiy <github@elsesiy.com>
2022-01-26 12:12:15 -0800

    Allow for pagination of StateVersionOutputs
    The current implementation does not return the pagination information needed to traverse all output pages

--- state_version.go
+++ state_version.go
@@ -40,7 +40,7 @@ type StateVersions interface {
 	Download(ctx context.Context, url string) ([]byte, error)
 
 	// Outputs retrieves all the outputs of a state version by its ID.
-	Outputs(ctx context.Context, svID string, options StateVersionOutputsListOptions) ([]*StateVersionOutput, error)
+	Outputs(ctx context.Context, svID string, options StateVersionOutputsListOptions) (*StateVersionOutputsList, error)
 }
 
 // stateVersions implements StateVersions.
@@ -262,7 +262,7 @@ type StateVersionOutputsListOptions struct {
 }
 
 // Outputs retrieves all the outputs of a state version by its ID.
-func (s *stateVersions) Outputs(ctx context.Context, svID string, options StateVersionOutputsListOptions) ([]*StateVersionOutput, error) {
+func (s *stateVersions) Outputs(ctx context.Context, svID string, options StateVersionOutputsListOptions) (*StateVersionOutputsList, error) {
 	if !validStringID(&svID) {
 		return nil, errors.New("invalid value for state version ID")
 	}
@@ -279,5 +279,5 @@ func (s *stateVersions) Outputs(ctx context.Context, svID string, options StateV
 		return nil, err
 	}
 
-	return sv.Items, nil
+	return sv, nil
 }
