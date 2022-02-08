29a3c510a3f03ea56bc0323d800b89bb32de2f70
Sebastian Rivera <sebastian.rivera@hashicorp.com>
2022-02-08 14:43:28 -0500

    Code documentation for workspace run tasks

--- workspace_run_task.go
+++ workspace_run_task.go
@@ -6,24 +6,34 @@ import (
 	"net/url"
 )
 
+// Compile-time proof of interface implementation
 var _ WorkspaceRunTasks = (*workspaceRunTasks)(nil)
 
+// WorkspaceRunTasks represent all the run task related methods in the context of a workspace that the Terraform Cloud/Enterprise API supports.
+// **Note: This API is still in BETA and subject to change.**
 type WorkspaceRunTasks interface {
+	// Add a run task to a workspace
 	Create(ctx context.Context, workspaceID string, options WorkspaceRunTaskCreateOptions) (*WorkspaceRunTask, error)
 
+	// List all run tasks for a workspace
 	List(ctx context.Context, workspaceID string, options *WorkspaceRunTaskListOptions) (*WorkspaceRunTaskList, error)
 
+	// Read a workspace run task by ID
 	Read(ctx context.Context, workspaceID string, workspaceTaskID string) (*WorkspaceRunTask, error)
 
+	// Update a workspace run task by ID
 	Update(ctx context.Context, workspaceID string, workspaceTaskID string, options WorkspaceRunTaskUpdateOptions) (*WorkspaceRunTask, error)
 
+	// Delete a workspace's run task by ID
 	Delete(ctx context.Context, workspaceID string, workspaceTaskID string) error
 }
 
+// workspaceRunTasks implements WorkspaceRunTasks
 type workspaceRunTasks struct {
 	client *Client
 }
 
+// WorkspaceRunTask represents a TFC/E run task that belongs to a workspace
 type WorkspaceRunTask struct {
 	ID               string               `jsonapi:"primary,workspace-tasks"`
 	EnforcementLevel TaskEnforcementLevel `jsonapi:"attr,enforcement-level"`
@@ -32,15 +42,18 @@ type WorkspaceRunTask struct {
 	Workspace *Workspace `jsonapi:"relation,workspace"`
 }
 
+// WorkspaceRunTaskList represents a list of workspace run tasks
 type WorkspaceRunTaskList struct {
 	*Pagination
 	Items []*WorkspaceRunTask
 }
 
+// WorkspaceRunTaskListOptions represents the set of options for listing workspace run tasks
 type WorkspaceRunTaskListOptions struct {
 	ListOptions
 }
 
+// List all run tasks attached to a workspace
 func (s *workspaceRunTasks) List(ctx context.Context, workspaceID string, options *WorkspaceRunTaskListOptions) (*WorkspaceRunTaskList, error) {
 	if !validStringID(&workspaceID) {
 		return nil, ErrInvalidWorkspaceID
@@ -61,6 +74,7 @@ func (s *workspaceRunTasks) List(ctx context.Context, workspaceID string, option
 	return rl, nil
 }
 
+// Read a workspace run task by ID
 func (s *workspaceRunTasks) Read(ctx context.Context, workspaceID string, workspaceTaskID string) (*WorkspaceRunTask, error) {
 	if !validStringID(&workspaceID) {
 		return nil, ErrInvalidWorkspaceID
@@ -89,10 +103,13 @@ func (s *workspaceRunTasks) Read(ctx context.Context, workspaceID string, worksp
 	return wr, nil
 }
 
+// WorkspaceRunTaskCreateOptions represents the set of options for creating a workspace run task
 type WorkspaceRunTaskCreateOptions struct {
-	Type             string               `jsonapi:"primary,workspace-tasks"`
+	Type string `jsonapi:"primary,workspace-tasks"`
+	// Required: The enforcement level for a run task
 	EnforcementLevel TaskEnforcementLevel `jsonapi:"attr,enforcement-level"`
-	RunTask          *RunTask             `jsonapi:"relation,task"`
+	// Required: The run task to attach to the workspace
+	RunTask *RunTask `jsonapi:"relation,task"`
 }
 
 func (o *WorkspaceRunTaskCreateOptions) valid() error {
@@ -103,6 +120,7 @@ func (o *WorkspaceRunTaskCreateOptions) valid() error {
 	return nil
 }
 
+// Create is used to attach a run task to a workspace, or in other words: create a workspace run task. The run task must exist in the workspace's organization.
 func (s *workspaceRunTasks) Create(ctx context.Context, workspaceID string, options WorkspaceRunTaskCreateOptions) (*WorkspaceRunTask, error) {
 	if !validStringID(&workspaceID) {
 		return nil, ErrInvalidWorkspaceID
@@ -127,11 +145,13 @@ func (s *workspaceRunTasks) Create(ctx context.Context, workspaceID string, opti
 	return wr, nil
 }
 
+// WorkspaceRunTaskUpdateOptions represent the set of options for updating a workspace run task.
 type WorkspaceRunTaskUpdateOptions struct {
 	Type             string               `jsonapi:"primary,workspace-tasks"`
 	EnforcementLevel TaskEnforcementLevel `jsonapi:"attr,enforcement-level,omitempty"`
 }
 
+// Update an existing workspace run task by ID
 func (s *workspaceRunTasks) Update(ctx context.Context, workspaceID string, workspaceTaskID string, options WorkspaceRunTaskUpdateOptions) (*WorkspaceRunTask, error) {
 	if !validStringID(&workspaceID) {
 		return nil, ErrInvalidWorkspaceID
@@ -160,6 +180,7 @@ func (s *workspaceRunTasks) Update(ctx context.Context, workspaceID string, work
 	return wr, nil
 }
 
+// Delete a workspace run task by ID
 func (s *workspaceRunTasks) Delete(ctx context.Context, workspaceID string, workspaceTaskID string) error {
 	if !validStringID(&workspaceID) {
 		return ErrInvalidWorkspaceID
