cf271630b5dbde1cc7272dce752e0ec920048627
Mario Minardi <mario.minardi@hashicorp.com>
2022-03-01 10:40:58 -0700

    Add missed 'fetching' status to configuration_version

--- configuration_version.go
+++ configuration_version.go
@@ -57,6 +57,7 @@ type ConfigurationStatus string
 const (
 	ConfigurationArchived ConfigurationStatus = "archived"
 	ConfigurationErrored  ConfigurationStatus = "errored"
+	ConfigurationFetching ConfigurationStatus = "fetching"
 	ConfigurationPending  ConfigurationStatus = "pending"
 	ConfigurationUploaded ConfigurationStatus = "uploaded"
 )
