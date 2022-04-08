1e1177a214c5fe764c3b027c038a068d9fb42782
Byron Wolfman <wolfman@hashicorp.com>
2022-04-08 10:54:06 -0300

    Move mistakenly-placed comment

--- variable_set.go
+++ variable_set.go
@@ -258,6 +258,7 @@ func (o *VariableSetApplyToWorkspacesOptions) valid() error {
 }
 
 // Apply variable set to workspaces in the supplied list.
+// Note: this method will return an error if the variable set has global = true.
 func (s *variableSets) ApplyToWorkspaces(ctx context.Context, variableSetID string, options *VariableSetApplyToWorkspacesOptions) error {
 	if !validStringID(&variableSetID) {
 		return ErrInvalidVariableSetID
@@ -310,7 +311,6 @@ func (s *variableSets) RemoveFromWorkspaces(ctx context.Context, variableSetID s
 }
 
 // VariableSetUpdateWorkspacesOptions represents a subset of update options specifically for applying variable sets to workspaces
-// Note: this method will return an error if the variable set has global = true.
 type VariableSetUpdateWorkspacesOptions struct {
 	// Type is a public field utilized by JSON:API to
 	// set the resource type via the field tag.
