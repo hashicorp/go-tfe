fe72e20901ff2f88d5944df5010e1e223ac69e17
Brandon Croft <brandon.croft@gmail.com>
2022-06-30 15:39:53 -0600

    feat: optionally send json outputs when creating state versions

--- state_version.go
+++ state_version.go
@@ -139,13 +139,13 @@ type StateVersionCreateOptions struct {
 	// Optional: Specifies the run to associate the state with.
 	Run *Run `jsonapi:"relation,run,omitempty"`
 
-	// Optional: The external, json representation of state data, base64 encoded.
-	// https://www.terraform.io/internals/json-format#state-representation
-	// Supplying this state representation can provide more details to the platform
+	// Optional: The external, json representation of state outputs, base64 encoded.
+	// https://www.terraform.io/internals/json-format#values-representation (the contents of the
+	// "outputs" attribute) Supplying detailed outputs will provide more details to the platform
 	// about the current terraform state.
 	//
 	// **Note**: This field is in BETA, subject to change and not widely available yet.
-	JSONState *string `jsonapi:"attr,json-state,omitempty"`
+	JSONStateOutputs *string `jsonapi:"attr,json-state-outputs,omitempty"`
 }
 
 // List all the state versions for a given workspace.
