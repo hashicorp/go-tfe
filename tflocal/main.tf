538638bb5d718594b785d8547112281387f3ecdc
Sebastian Rivera <sebastian.rivera@hashicorp.com>
2022-07-13 17:07:39 -0400

    Initial tflocal configuration for CI
    This configuration makes use of the tflocal module that provisions the necessary TFE installation and AWS resources to run CI.

---
+++ tflocal/main.tf
@@ -0,0 +1,28 @@
+module "tflocal" {
+  source                       = "app.terraform.io/hashicorp-v2/tflocal-cloud/aws"
+  version                      = "0.3.0"
+  tflocal_cloud                = "true"
+  tflocal_cloud_admin_password = var.tflocal_cloud_admin_password
+  tflocal_cloud_admin_token    = var.tflocal_cloud_admin_token
+  git_branch                   = var.git_branch
+  tfe_ref                      = var.tfe_ref
+  ngrok_domain                 = var.ngrok_domain
+  ngrok_tunnel_token           = var.ngrok_tunnel_token
+  private_github_token         = var.private_github_token
+  private_github_user          = var.private_github_user
+  gem_contribsys_key           = var.gem_contribsys_key
+  ejson_file_name              = var.ejson_file_name
+  ejson_file_content           = var.ejson_file_content
+  quay_username                = var.quay_username
+  quay_password                = var.quay_password
+  circleci_token               = var.circleci_token
+  update_circleci              = var.update_circleci
+  run_cleanup_script           = var.run_cleanup_script
+  env                          = var.env
+
+  tags = {
+    Codebase  = "hashicorp/go-tfe"
+    Purpose   = "go-tfe integration tests"
+    Workspace = terraform.workspace
+  }
+}
