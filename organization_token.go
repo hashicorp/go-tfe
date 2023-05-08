7dc05f6a113ff6d335e3d0e730de1576145a3516
Julianna Tetreault <julianna.tetreault@hashicorp.com>
2023-05-08 17:12:01 -0600

    Fix typo in organization_token.go

--- organization_token.go
+++ organization_token.go
@@ -50,7 +50,7 @@ type OrganizationToken struct {
 // OrganizationTokenCreateOptions contains the options for creating an organization token.
 type OrganizationTokenCreateOptions struct {
 	// Optional: The token's expiration date.
-	// Heads up: his feature is available in TFE release v202305-1 and later
+	// This feature is available in TFE release v202305-1 and later
 	ExpiredAt *time.Time `jsonapi:"attr,expired-at,iso8601,omitempty"`
 }
 
