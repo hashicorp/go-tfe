be174044fce98859f980b022648eb9a1cdc25eec
hwatkins05-hashicorp <hannah.watkins@hashicorp.com>
2025-05-30 14:03:44 -0700

    Drafted tests for speculative option on stack sources

--- stack_source_integration_test.go
+++ stack_source_integration_test.go
@@ -8,6 +8,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -27,12 +28,13 @@ func TestStackSourceCreateUploadAndRead(t *testing.T) {
 		Project: orgTest.DefaultProject,
 		Name:    "test-stack",
 		VCSRepo: &StackVCSRepoOptions{
-			Identifier:   "hashicorp-guides/pet-nulls-stack",
+			Identifier:   "hwatkins05-hashicorp/pet-nulls-stack",
 			OAuthTokenID: oauthClient.OAuthTokens[0].ID,
 		},
 	})
 	require.NoError(t, err)
 
+	// Should I modify create stack source options to include speculative enabled?
 	ss, err := client.StackSources.CreateAndUpload(ctx, stack.ID, "test-fixtures/stack-source", &CreateStackSourceOptions{
 		SelectedDeployments: []string{"simple"},
 	})
@@ -65,4 +67,95 @@ func TestStackSourceCreateUploadAndRead(t *testing.T) {
 	case <-ctx.Done():
 		require.Fail(t, "timed out waiting for stack source to be processed")
 	}
+}
+
+// TO-DO: Can I just add a function here to test the stack source upload? Specifically speculative uploads?
+// This should not have errors, since it is a speculative run on a VCS upload.
+func TestStackSourceSpeculativeVCSUpload(t *testing.T) {
+	skipUnlessBeta(t)
+
+	client := testClient(t)
+	ctx := context.Background()
+
+	orgTest, orgTestCleanup := createOrganization(t, client)
+	t.Cleanup(orgTestCleanup)
+
+	oauthClient, cleanup := createOAuthClient(t, client, orgTest, nil)
+	t.Cleanup(cleanup)
+
+	stack, err := client.Stacks.Create(ctx, StackCreateOptions{
+		Project: orgTest.DefaultProject,
+		Name:    "test-stack",
+		VCSRepo: &StackVCSRepoOptions{
+			Identifier:   "hwatkins05-hashicorp/pet-nulls-stack",
+			OAuthTokenID: oauthClient.OAuthTokens[0].ID,
+		},
+	})
+	require.NoError(t, err)
+
+	options := &CreateStackSourceOptions{
+		SelectedDeployments: []string{"simple"},
+		SpeculativeEnabled:  true,
+	}
+	ss, err := client.StackSources.CreateAndUpload(ctx, stack.ID, "test-fixtures/stack-source", options)
+	require.NoError(t, err)
+	require.NotNil(t, ss)
+	require.NotNil(t, ss.UploadURL)
+	assert.Equal(t, options.SpeculativeEnabled, ss.Stack.SpeculativeEnabled)
+}
+
+// This should fail because it is a non-speculative run on a VCS upload.
+func TestStackSourceNonSpeculativeVCSUpload(t *testing.T) {
+	skipUnlessBeta(t)
+
+	client := testClient(t)
+	ctx := context.Background()
+
+	orgTest, orgTestCleanup := createOrganization(t, client)
+	t.Cleanup(orgTestCleanup)
+
+	oauthClient, cleanup := createOAuthClient(t, client, orgTest, nil)
+	t.Cleanup(cleanup)
+
+	stack, err := client.Stacks.Create(ctx, StackCreateOptions{
+		Project: orgTest.DefaultProject,
+		Name:    "test-stack",
+		VCSRepo: &StackVCSRepoOptions{
+			Identifier:   "hwatkins05-hashicorp/pet-nulls-stack",
+			OAuthTokenID: oauthClient.OAuthTokens[0].ID,
+		},
+	})
+	require.NoError(t, err)
+
+	_, err = client.StackSources.CreateAndUpload(ctx, stack.ID, "test-fixtures/stack-source", &CreateStackSourceOptions{
+		SelectedDeployments: []string{"simple"},
+		SpeculativeEnabled: false,
+	})
+	require.Error(t, err)
+}
+
+// This is a speculative run to HCP Terraform Cloud, so it should not error.
+func TestStackSourceSpeculativeManualUpload(t *testing.T) {
+	skipUnlessBeta(t)
+
+	client := testClient(t)
+	ctx := context.Background()
+
+	orgTest, orgTestCleanup := createOrganization(t, client)
+	t.Cleanup(orgTestCleanup)
+
+	stack, err := client.Stacks.Create(ctx, StackCreateOptions{
+		Project: &Project{
+			ID: orgTest.DefaultProject.ID,
+		},
+	})
+	require.NoError(t, err)
+
+	ss, err := client.StackSources.CreateAndUpload(ctx, stack.ID, "test-fixtures/stack-source", &CreateStackSourceOptions{
+		SelectedDeployments: []string{"simple"},
+		SpeculativeEnabled:  true,
+	})
+	require.NoError(t, err)
+	require.NotNil(t, ss)
+	require.NotNil(t, ss.UploadURL)
 }
