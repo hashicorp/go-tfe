b6044903679b0f5c10c111481f46eb9a4b7e42d0
Chris Trombley <chris.trombley@hashicorp.com>
2025-06-17 12:36:15 -0700

    tests: get tests passing

--- stack_deployment_steps.go
+++ stack_deployment_steps.go
@@ -7,6 +7,7 @@ import (
 	"context"
 	"fmt"
 	"net/url"
+	"time"
 )
 
 // StackDeploymentSteps describes all the stacks deployment step-related methods that the
@@ -15,16 +16,40 @@ import (
 // release notes.
 type StackDeploymentSteps interface {
 	// List returns the stack deployment steps for a stack deployment run.
-	List(ctx context.Context, stackDeploymentRunID string) (*StackDeploymentStepList, error)
+	List(ctx context.Context, stackDeploymentRunID string, opts *StackDeploymentStepsListOptions) (*StackDeploymentStepList, error)
 	Read(ctx context.Context, stackDeploymentStepID string) (*StackDeploymentStep, error)
+}
+
+// StackDeploymentStep represents a step from a stack deployment
+type StackDeploymentStep struct {
+	// Attributes
+	ID        string    `jsonapi:"primary,stacks-deployment-steps"`
+	Status    string    `jsonapi:"attr,status"`
+	CreatedAt time.Time `jsonapi:"attr,created-at,iso8601"`
+	UpdatedAt time.Time `jsonapi:"attr,created-at,iso8601"`
+
+	// Relationships
+	StackDeploymentRun *StackDeploymentRun `jsonapi:"relation,stack-deployment-run"`
+}
+
+// StackDeploymentStepList represents a list of stack deployment steps
+type StackDeploymentStepList struct {
+	*Pagination
+	Items []*StackDeploymentStep
 }
 
 type stackDeploymentSteps struct {
 	client *Client
 }
 
-func (s stackDeploymentSteps) List(ctx context.Context, stackDeploymentRunID string) (*StackDeploymentStepList, error) {
-	req, err := s.client.NewRequest("GET", fmt.Sprintf("stack-deployment-runs/%s/stack-deployment-steps", url.PathEscape(stackDeploymentRunID)), nil)
+// StackDeploymentStepsListOptions represents the options for listing stack
+// deployment steps.
+type StackDeploymentStepsListOptions struct {
+	ListOptions
+}
+
+func (s stackDeploymentSteps) List(ctx context.Context, stackDeploymentRunID string, opts *StackDeploymentStepsListOptions) (*StackDeploymentStepList, error) {
+	req, err := s.client.NewRequest("GET", fmt.Sprintf("stack-deployment-runs/%s/stack-deployment-steps", url.PathEscape(stackDeploymentRunID)), opts)
 	if err != nil {
 		return nil, err
 	}
--- stack_deployment_steps_integration_test.go
+++ stack_deployment_steps_integration_test.go
@@ -7,6 +7,7 @@ import (
 	"context"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -47,15 +48,115 @@ func TestStackDeploymentStepsList(t *testing.T) {
 
 	sdg := stackDeploymentGroups.Items[0]
 
-	stackDeploymentRuns, err := client.StackDeploymentRuns.List(ctx, sdg.ID)
+	stackDeploymentRuns, err := client.StackDeploymentRuns.List(ctx, sdg.ID, nil)
 	require.NoError(t, err)
 	require.NotEmpty(t, stackDeploymentRuns)
 
 	sdr := stackDeploymentRuns.Items[0]
-	steps, err := client.StackDeploymentSteps.List(ctx, sdr.ID)
-	require.NoError(t, err)
-	require.NotEmpty(t, steps)
 
-	step := steps.Items[0]
-	require.NotNil(t, step)
+	t.Run("List with invalid stack deployment run ID", func(t *testing.T) {
+		t.Parallel()
+
+		_, err := client.StackDeploymentSteps.List(ctx, "", nil)
+		assert.Error(t, err)
+	})
+
+	t.Run("List without options", func(t *testing.T) {
+		t.Parallel()
+
+		steps, err := client.StackDeploymentSteps.List(ctx, sdr.ID, nil)
+		assert.NoError(t, err)
+		assert.NotEmpty(t, steps)
+
+		step := steps.Items[0]
+
+		assert.NotNil(t, step)
+		assert.NotNil(t, step.ID)
+		assert.NotNil(t, step.Status)
+
+		require.NotNil(t, step.StackDeploymentRun)
+		assert.Equal(t, step.StackDeploymentRun.ID, sdr.ID)
+		assert.Equal(t, step.StackDeploymentRun.Status, sdr.Status)
+		assert.Equal(t, step.StackDeploymentRun.Deployment, sdr.Deployment)
+	})
+
+	t.Run("List with pagination", func(t *testing.T) {
+		t.Parallel()
+
+		steps, err := client.StackDeploymentSteps.List(ctx, sdr.ID, &StackDeploymentStepsListOptions{
+			ListOptions: ListOptions{
+				PageNumber: 1,
+				PageSize:   10,
+			},
+		})
+		assert.NoError(t, err)
+		assert.NotEmpty(t, steps)
+
+		step := steps.Items[0]
+
+		assert.NotNil(t, step)
+		assert.NotNil(t, step.ID)
+		assert.NotNil(t, step.Status)
+
+		require.NotNil(t, step.StackDeploymentRun)
+		assert.Equal(t, step.StackDeploymentRun.ID, sdr.ID)
+		assert.Equal(t, step.StackDeploymentRun.Status, sdr.Status)
+		assert.Equal(t, step.StackDeploymentRun.Deployment, sdr.Deployment)
+	})
+}
+
+func TestStackDeploymentStepsRead(t *testing.T) {
+	skipUnlessBeta(t)
+
+	client := testClient(t)
+	ctx := context.Background()
+
+	orgTest, orgTestCleanup := createOrganization(t, client)
+	t.Cleanup(orgTestCleanup)
+
+	oauthClient, cleanup := createOAuthClient(t, client, orgTest, nil)
+	t.Cleanup(cleanup)
+
+	stack, err := client.Stacks.Create(ctx, StackCreateOptions{
+		Project: orgTest.DefaultProject,
+		Name:    "test-stack",
+		VCSRepo: &StackVCSRepoOptions{
+			Identifier:   "hashicorp-guides/pet-nulls-stack",
+			OAuthTokenID: oauthClient.OAuthTokens[0].ID,
+			Branch:       "main",
+		},
+	})
+	require.NoError(t, err)
+	require.NotNil(t, stack)
+
+	stackUpdated, err := client.Stacks.UpdateConfiguration(ctx, stack.ID)
+	require.NoError(t, err)
+	require.NotNil(t, stackUpdated)
+
+	stack = pollStackDeployments(t, ctx, client, stackUpdated.ID)
+	require.NotNil(t, stack.LatestStackConfiguration)
+
+	stackDeploymentGroups, err := client.StackDeploymentGroups.List(ctx, stack.LatestStackConfiguration.ID, nil)
+	require.NoError(t, err)
+	require.NotEmpty(t, stackDeploymentGroups)
+
+	sdg := stackDeploymentGroups.Items[0]
+
+	stackDeploymentRuns, err := client.StackDeploymentRuns.List(ctx, sdg.ID, nil)
+	require.NoError(t, err)
+	require.NotEmpty(t, stackDeploymentRuns)
+
+	sdr := stackDeploymentRuns.Items[0]
+
+	t.Run("Read with valid ID", func(t *testing.T) {
+		step, err := client.StackDeploymentSteps.Read(ctx, sdr.ID)
+		assert.NoError(t, err)
+		assert.Equal(t, step.ID, sdg.ID)
+		assert.Equal(t, step.Status, sdg.Status)
+	})
+
+	t.Run("Read with invalid ID", func(t *testing.T) {
+		_, err := client.StackDeploymentGroups.Read(ctx, "")
+		require.Error(t, err)
+	})
 }
