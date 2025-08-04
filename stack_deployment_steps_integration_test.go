fdd322530e77633e45759813d1abf45464140b5a
Shweta <35878561+shwetamurali@users.noreply.github.com>
2025-08-04 11:17:25 -0400

    Update stack_deployment_steps_integration_test.go

--- stack_deployment_steps_integration_test.go
+++ stack_deployment_steps_integration_test.go
@@ -157,14 +157,6 @@ func TestStackDeploymentStepsRead(t *testing.T) {
 		assert.NotEmpty(t, sds.Status)
 	})
 
-	t.Run("Read UploadURL field serialization", func(t *testing.T) {
-		sds, err := client.StackDeploymentSteps.Read(ctx, step.ID)
-		assert.NoError(t, err)
-		assert.NotNil(t, sds)
-
-		assert.IsType(t, "", sds.UploadURL)
-	})
-
 	t.Run("Read with invalid ID", func(t *testing.T) {
 		_, err := client.StackDeploymentSteps.Read(ctx, "")
 		require.Error(t, err)
