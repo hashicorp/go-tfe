932d6a469c01b81faf8e8fcba7d13ce77b762dfd
Helen Jiang <helen.jiang@hashicorp.com>
2025-08-14 16:32:14 -0400

    gcp_oidc_configuration_integration_test.go

--- gcp_oidc_configuration_integration_test.go
+++ gcp_oidc_configuration_integration_test.go
@@ -8,44 +8,185 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestGCPOIDCConfigurationsCreateReadUpdateDelete(t *testing.T) {
+func TestGCPOIDCConfigurationCreate(t *testing.T) {
+	skipIfEnterprise(t)
+
 	client := testClient(t)
 	ctx := context.Background()
 
 	orgTest, orgTestCleanup := createOrganization(t, client)
 	t.Cleanup(orgTestCleanup)
 
-	// Using "silly_name" because of the hyok feature flag.
-	// Put in the name of the organization you want to test with.
-	orgTest, err := client.Organizations.Read(ctx, "silly_name")
+	t.Run("with valid options", func(t *testing.T) {
+		opts := GCPOIDCConfigurationCreateOptions{
+			ServiceAccountEmail:  "updated-service-account@example.iam.gserviceaccount.com",
+			ProjectNumber:        "123456789012",
+			WorkloadProviderName: randomString(t),
+			Organization: &Organization{
+				Name: orgTest.Name,
+			},
+		}
 
-	create_gcp_oidc_configuration, err := client.GCPOIDCConfigurations.Create(ctx, orgTest.Name, GCPOIDCConfigurationCreateOptions{
-		ServiceAccountEmail:  "service-account@example.iam.gserviceaccount.com",
-		ProjectNumber:        "123456789012",
-		WorkloadProviderName: "rocket",
-		Organization: &Organization{
-			Name: orgTest.Name,
-		},
+		oidcConfig, err := client.GCPOIDCConfigurations.Create(ctx, orgTest.Name, opts)
+		require.NoError(t, err)
+		require.NotNil(t, oidcConfig)
+		assert.Equal(t, oidcConfig.ServiceAccountEmail, opts.ServiceAccountEmail)
+		assert.Equal(t, oidcConfig.ProjectNumber, opts.ProjectNumber)
+		assert.Equal(t, oidcConfig.WorkloadProviderName, opts.WorkloadProviderName)
 	})
-	require.NoError(t, err)
-	require.NotNil(t, create_gcp_oidc_configuration)
 
-	read_gcp_oidc_configuration, err := client.GCPOIDCConfigurations.Read(ctx, create_gcp_oidc_configuration.ID)
-	require.NoError(t, err)
-	require.NotNil(t, read_gcp_oidc_configuration)
+	t.Run("missing workload provider name", func(t *testing.T) {
+		opts := GCPOIDCConfigurationCreateOptions{
+			ServiceAccountEmail: "updated-service-account@example.iam.gserviceaccount.com",
+			ProjectNumber:       "123456789012",
+			Organization: &Organization{
+				Name: orgTest.Name,
+			},
+		}
 
-	update_gcp_oidc_configuration, err := client.GCPOIDCConfigurations.Update(ctx, create_gcp_oidc_configuration.ID, GCPOIDCConfigurationUpdateOptions{
-		ServiceAccountEmail:  "updated-service-account@example.iam.gserviceaccount.com",
-		ProjectNumber:        "987654321012",
-		WorkloadProviderName: "rocket-updated",
+		_, err := client.GCPOIDCConfigurations.Create(ctx, orgTest.Name, opts)
+		assert.ErrorIs(t, err, ErrRequiredWorkloadProviderName)
 	})
-	require.NoError(t, err)
-	require.NotNil(t, update_gcp_oidc_configuration)
-	assert.Equal(t, create_gcp_oidc_configuration.ID, update_gcp_oidc_configuration.ID)
-	assert.Equal(t, "updated-service-account@example.iam.gserviceaccount.com", update_gcp_oidc_configuration.ServiceAccountEmail)
-	assert.Equal(t, "987654321012", update_gcp_oidc_configuration.ProjectNumber)
-	assert.Equal(t, "rocket-updated", update_gcp_oidc_configuration.WorkloadProviderName)
 
-	err = client.GCPOIDCConfigurations.Delete(ctx, create_gcp_oidc_configuration.ID)
-	require.NoError(t, err)
+	t.Run("missing service account email", func(t *testing.T) {
+		opts := GCPOIDCConfigurationCreateOptions{
+			ProjectNumber:        "123456789012",
+			WorkloadProviderName: randomString(t),
+			Organization: &Organization{
+				Name: orgTest.Name,
+			},
+		}
+
+		_, err := client.GCPOIDCConfigurations.Create(ctx, orgTest.Name, opts)
+		assert.ErrorIs(t, err, ErrRequiredServiceAccountEmail)
+	})
+
+	t.Run("missing project number", func(t *testing.T) {
+		opts := GCPOIDCConfigurationCreateOptions{
+			ServiceAccountEmail:  "updated-service-account@example.iam.gserviceaccount.com",
+			WorkloadProviderName: randomString(t),
+			Organization: &Organization{
+				Name: orgTest.Name,
+			},
+		}
+
+		_, err := client.GCPOIDCConfigurations.Create(ctx, orgTest.Name, opts)
+		assert.ErrorIs(t, err, ErrRequiredProjectNumber)
+	})
+}
+
+func TestGCPOIDCConfigurationRead(t *testing.T) {
+	skipIfEnterprise(t)
+
+	client := testClient(t)
+	ctx := context.Background()
+
+	orgTest, orgTestCleanup := createOrganization(t, client)
+	t.Cleanup(orgTestCleanup)
+
+	oidcConfig, oidcConfigCleanup := createGCPOIDCConfiguration(t, client, orgTest)
+	t.Cleanup(oidcConfigCleanup)
+
+	t.Run("fetch existing configuration", func(t *testing.T) {
+		fetched, err := client.GCPOIDCConfigurations.Read(ctx, oidcConfig.ID)
+		require.NoError(t, err)
+		require.NotEmpty(t, fetched)
+	})
+
+	t.Run("fetching non-existing configuration", func(t *testing.T) {
+		_, err := client.GCPOIDCConfigurations.Read(ctx, "gcpoidc-notreal")
+		assert.ErrorIs(t, err, ErrResourceNotFound)
+	})
+}
+
+func TestGCPOIDCConfigurationUpdate(t *testing.T) {
+	skipIfEnterprise(t)
+
+	client := testClient(t)
+	ctx := context.Background()
+
+	orgTest, orgTestCleanup := createOrganization(t, client)
+	t.Cleanup(orgTestCleanup)
+
+	oidcConfig, oidcConfigCleanup := createGCPOIDCConfiguration(t, client, orgTest)
+	t.Cleanup(oidcConfigCleanup)
+
+	t.Run("with valid options", func(t *testing.T) {
+		opts := GCPOIDCConfigurationUpdateOptions{
+			ServiceAccountEmail:  "updated-service-account@example.iam.gserviceaccount.com",
+			ProjectNumber:        "123456789012",
+			WorkloadProviderName: randomString(t),
+			Organization: &Organization{
+				Name: orgTest.Name,
+			},
+		}
+
+		updated, err := client.GCPOIDCConfigurations.Update(ctx, oidcConfig.ID, opts)
+		require.NoError(t, err)
+		require.NotNil(t, updated)
+		assert.Equal(t, updated.ServiceAccountEmail, opts.ServiceAccountEmail)
+		assert.Equal(t, updated.ProjectNumber, opts.ProjectNumber)
+		assert.Equal(t, updated.WorkloadProviderName, opts.WorkloadProviderName)
+	})
+
+	t.Run("missing workload provider name", func(t *testing.T) {
+		opts := GCPOIDCConfigurationUpdateOptions{
+			ServiceAccountEmail: "updated-service-account@example.iam.gserviceaccount.com",
+			ProjectNumber:       "123456789012",
+			Organization: &Organization{
+				Name: orgTest.Name,
+			},
+		}
+
+		_, err := client.GCPOIDCConfigurations.Update(ctx, oidcConfig.ID, opts)
+		assert.ErrorIs(t, err, ErrRequiredWorkloadProviderName)
+	})
+
+	t.Run("missing service account email", func(t *testing.T) {
+		opts := GCPOIDCConfigurationUpdateOptions{
+			ProjectNumber:        "123456789012",
+			WorkloadProviderName: randomString(t),
+			Organization: &Organization{
+				Name: orgTest.Name,
+			},
+		}
+
+		_, err := client.GCPOIDCConfigurations.Update(ctx, oidcConfig.ID, opts)
+		assert.ErrorIs(t, err, ErrRequiredServiceAccountEmail)
+	})
+
+	t.Run("missing project number", func(t *testing.T) {
+		opts := GCPOIDCConfigurationUpdateOptions{
+			ServiceAccountEmail:  "updated-service-account@example.iam.gserviceaccount.com",
+			WorkloadProviderName: randomString(t),
+			Organization: &Organization{
+				Name: orgTest.Name,
+			},
+		}
+
+		_, err := client.GCPOIDCConfigurations.Update(ctx, oidcConfig.ID, opts)
+		assert.ErrorIs(t, err, ErrRequiredProjectNumber)
+	})
+}
+
+func TestGCPOIDCConfigurationDelete(t *testing.T) {
+	skipIfEnterprise(t)
+
+	client := testClient(t)
+	ctx := context.Background()
+
+	orgTest, orgTestCleanup := createOrganization(t, client)
+	t.Cleanup(orgTestCleanup)
+
+	oidcConfig, _ := createGCPOIDCConfiguration(t, client, orgTest)
+
+	t.Run("delete existing configuration", func(t *testing.T) {
+		err := client.GCPOIDCConfigurations.Delete(ctx, oidcConfig.ID)
+		require.NoError(t, err)
+	})
+
+	t.Run("fetching non-existing configuration", func(t *testing.T) {
+		err := client.GCPOIDCConfigurations.Delete(ctx, "gcpoidc-notreal")
+		require.ErrorIs(t, err, ErrResourceNotFound)
+	})
 }
