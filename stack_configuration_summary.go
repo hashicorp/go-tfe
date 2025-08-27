3a9ee956231e6c778b32793c50f30ab9aff9b78d
hwatkins05-hashicorp <hannah.watkins@hashicorp.com>
2025-08-27 14:08:31 -0500

    bare functionality for listing summaries

---
+++ stack_configuration_summary.go
@@ -0,0 +1,49 @@
+package tfe
+
+import (
+	"context"
+	"fmt"
+	"net/url"
+)
+
+type StackConfigurationSummaries interface {
+	//ListStackConfigurationSummaries lists all the stack configuration summaries for a stack.
+	ListStackConfigurationSummaries(ctx context.Context, stackID string) (*StackConfigurationSummaryList, error)
+}
+
+type stackConfigurationSummaries struct {
+	client *Client
+}
+
+var _ StackConfigurationSummaries = &stackConfigurationSummaries{}
+
+type StackConfigurationSummaryList struct {
+	*Pagination
+	Items []*StackConfigurationSummary
+}
+
+type StackConfigurationSummary struct {
+	ID             string
+	Type           string
+	Status         string
+	SequenceNumber int
+}
+
+func (s stackConfigurationSummaries) ListStackConfigurationSummaries(ctx context.Context, stackID string) (*StackConfigurationSummaryList, error) {
+	if !validStringID(&stackID) {
+		return nil, fmt.Errorf("invalid stack ID: %s", stackID)
+	}
+
+	req, err := s.client.NewRequest("GET", fmt.Sprintf("stacks/%s/stack-configuration-summaries", url.PathEscape(stackID)), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	scl := &StackConfigurationSummaryList{}
+	err = req.Do(ctx, scl)
+	if err != nil {
+		return nil, err
+	}
+
+	return scl, nil
+}
