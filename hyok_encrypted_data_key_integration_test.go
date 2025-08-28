8b0c086aa8baa2147ac437837b1b8f5d7fd16151
Helen Jiang <50344290+helenjw@users.noreply.github.com>
2025-08-28 15:34:06 -0400

    Add support for Customer Key Version and Encrypted Data Keys (#1203)
    Co-authored-by: Jarrett Spiker <jarrett.spiker@hashicorp.com>

---
+++ hyok_encrypted_data_key_integration_test.go
@@ -0,0 +1,28 @@
+package tfe
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+// These tests are intended for local execution only, as data encryption keys for HYOK requires specific conditions
+// for tests to run successfully. To test locally:
+// 1. Follow the instructions outlined in hyok_configuration_integration_test.go.
+// 2. Set hyokEncryptedDataKeyID to the ID of an existing data encryption key
+
+func TestHYOKEncryptedDataKeyRead(t *testing.T) {
+	if skipHYOKIntegrationTests {
+		t.Skip()
+	}
+
+	client := testClient(t)
+	ctx := context.Background()
+
+	t.Run("read an existing encrypted data key", func(t *testing.T) {
+		hyokEncryptedDataKeyID := ""
+		_, err := client.HYOKEncryptedDataKeys.Read(ctx, hyokEncryptedDataKeyID)
+		require.NoError(t, err)
+	})
+}
