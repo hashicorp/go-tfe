6ea0fe9b01a722127d6629a35373d46b1c48760d
Britni Alexander <britni.alexander@hashicorp.com>
2025-09-03 10:57:54 -0400

    sanity check

--- helper_test.go
+++ helper_test.go
@@ -280,7 +280,7 @@ func createAgent(t *testing.T, client *Client, org *Organization, agentPool *Age
 		defaultName := "test-agent"
 		name = &defaultName
 	}
-	fmt.Println(name)
+	fmt.Println(*name)
 
 	upgradeOrganizationSubscription(t, client, org)
 
