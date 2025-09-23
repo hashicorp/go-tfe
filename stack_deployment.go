aa96923b20d2791a1c5019d144d6f8ae0c6ed0f1
Chris Trombley <chris.trombley@hashicorp.com>
2025-09-23 15:55:41 -0700

    feat: add StackDeployments resource

---
+++ stack_deployment.go
@@ -0,0 +1,63 @@
+package tfe
+
+import (
+	"context"
+	"fmt"
+	"net/url"
+	"time"
+)
+
+type StackDeployments interface {
+	// List returns a list of stack deployments for a given stack.
+	List(ctx context.Context, stackID string, opts *StackDeploymentListOptions) (*StackDeploymentList, error)
+}
+
+type StackDeployment struct {
+	// Attributes
+	ID            string    `jsonapi:"primary,stack-deployments"`
+	Status        string    `jsonapi:"attr,status"`
+	Name          string    `jsonapi:"attr,name"`
+	DeployedAt    time.Time `jsonapi:"attr,deployed-at,iso8601"`
+	ErrorsCount   int       `jsonapi:"attr,errors-count"`
+	WarningsCount int       `jsonapi:"attr,warnings-count"`
+	PausedCount   int       `jsonapi:"attr,paused-count"`
+	HasEmptyState bool      `jsonapi:"attr,has-empty-state"`
+
+	// Relationships
+	Stack              *Stack              `jsonapi:"relation,stack"`
+	CurrentStackState  *StackState         `jsonapi:"relation,current-stack-state"`
+	StackConfiguration *StackConfiguration `jsonapi:"relation,stack-configuration"`
+	LatestStackPlan    *StackPlan          `jsonapi:"relation,latest-stack-plan"`
+	Diagnostics        []*StackDiagnostic  `jsonapi:"relation,stack-diagnostics"`
+}
+
+type stackDeployments struct {
+	client *Client
+}
+
+type StackDeploymentListOptions struct {
+	ListOptions
+}
+
+type StackDeploymentList struct {
+	*Pagination
+	Items []*StackDeployment
+}
+
+func (s stackDeployments) List(ctx context.Context, stackID string, opts *StackDeploymentListOptions) (*StackDeploymentList, error) {
+	if !validStringID(&stackID) {
+		return nil, ErrInvalidStackID
+	}
+
+	req, err := s.client.NewRequest("GET", fmt.Sprintf("stack-deployments/%s", url.PathEscape(stackID)), opts)
+	if err != nil {
+		return nil, err
+	}
+
+	var deployments *StackDeploymentList
+	if err := req.Do(ctx, deployments); err != nil {
+		return nil, err
+	}
+
+	return deployments, nil
+}
---
+++ stack_deployment_integration_test.go
@@ -0,0 +1,66 @@
+package tfe
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestStackDeploymentsList(t *testing.T) {
+	skipUnlessBeta(t)
+
+	client := testClient(t)
+	ctx := context.Background()
+
+	orgTest, orgTestCleanup := createOrganization(t, client)
+	t.Cleanup(orgTestCleanup)
+
+	oauthClient, cleanup := createOAuthClient(t, client, orgTest, nil)
+	t.Cleanup(cleanup)
+
+	stack, err := client.Stacks.Create(ctx, StackCreateOptions{
+		Name: "aa-test-stack",
+		VCSRepo: &StackVCSRepoOptions{
+			Identifier:   "ctrombley/linked-stacks-demo-network",
+			OAuthTokenID: oauthClient.OAuthTokens[0].ID,
+		},
+		Project: &Project{
+			ID: orgTest.DefaultProject.ID,
+		},
+	})
+	require.NoError(t, err)
+	require.NotNil(t, stack)
+
+	stackUpdated, err := client.Stacks.FetchLatestFromVcs(ctx, stack.ID)
+	require.NoError(t, err)
+	require.NotNil(t, stackUpdated)
+
+	stackUpdated = pollStackDeploymentGroups(t, ctx, client, stackUpdated.ID)
+	require.NotNil(t, stackUpdated.LatestStackConfiguration)
+
+	t.Run("List with valid options", func(t *testing.T) {
+		opts := &StackDeploymentListOptions{
+			ListOptions: ListOptions{
+				PageNumber: 1,
+				PageSize:   1,
+			},
+		}
+		sdl, err := client.StackDeployments.List(ctx, stackUpdated.ID, opts)
+		require.NoError(t, err)
+		require.NotNil(t, sdl)
+		require.Len(t, sdl.Items, 1)
+	})
+
+	t.Run("List with invalid options", func(t *testing.T) {
+		opts := &StackDeploymentListOptions{
+			ListOptions: ListOptions{
+				PageNumber: -1,
+				PageSize:   0,
+			},
+		}
+
+		_, err := client.StackDeployments.List(ctx, stackUpdated.ID, opts)
+		require.Error(t, err)
+	})
+}
