c232d8f472ae35d9067109e979179aac22446ade
Chris Trombley <chris.trombley@hashicorp.com>
2025-12-19 15:58:30 -0800

    tests: extend timeout

--- workspace_resources_integration_test.go
+++ workspace_resources_integration_test.go
@@ -5,6 +5,7 @@ package tfe
 
 import (
 	"context"
+	"flag"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -13,6 +14,11 @@ import (
 
 func TestWorkspaceResourcesList(t *testing.T) {
 	t.Parallel()
+
+	// Once go 1.22+ is required, replace with:
+	// t.SetTimeout(30 * time.Minute)
+	flag.Set("test.timeout", "30m0s")
+
 	client := testClient(t)
 	ctx := context.Background()
 
