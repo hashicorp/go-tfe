02c8dcba2299140082ce50ce16e8871ff8d63e48
Arun Ramakrishnan <arun@ibm.com>
2026-02-04 13:24:31 -0500

    [TF-33692] Add SpeculativeEnabled in create/update options for a Stack

--- stack_integration_test.go
+++ stack_integration_test.go
@@ -39,7 +39,8 @@ func TestStackCreateAndList(t *testing.T) {
 		Project: &Project{
 			ID: orgTest.DefaultProject.ID,
 		},
-		Migration: Bool(true),
+		Migration:          Bool(true),
+		SpeculativeEnabled: Bool(true),
 	})
 
 	require.NoError(t, err)
